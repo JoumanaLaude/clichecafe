0fb907b37e805c34fd4d8cf578d26a011ddf014d
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2021-05-28 20:05:19 -0400

    added JS for reservation phone number input

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,10 @@ $("#hoursButton").click(function(){
    $("#hoursModal").modal("show");
 });
 
+const resPhone = document.getElementById("phoneNumber");
+resPhone.addEventListener("keyup", fixLength);
+function fixLength() {
+  if(resPhone.value.length >= 12){
+   resPhone.value = resPhone.value.slice(0,12);
+ }
+}
